62e36da01d0365d811ef79569682a9456fc0b7cc
Auritro <22303012@gmail.com>
2026-02-23 00:00:24 +0600

    footer added without data

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -1,7 +1,11 @@
-import { Home, Inbox, Calendar, Search, Settings, Plus } from "lucide-react";
+import { Home, Inbox, Calendar, Search, Settings, Plus, User, User2, ChevronUp } from "lucide-react";
 import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupAction, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
 import Link from "next/link";
 import Image from "next/image";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Separator } from "./ui/separator";
+
+
 
 
 const items = [
@@ -14,27 +18,31 @@ const items = [
 
 const AppSidebar = () => {
     return (
-        <Sidebar>
+        <Sidebar collapsible="icon" variant="floating">
             <SidebarHeader>
                 <SidebarMenu>
                     <SidebarMenuItem>
                         <SidebarMenuButton asChild>
                             <Link href={"/"}>
-                            <Image src="/logo.svg" alt = "logo" height={20} width={20}/>
-
+                            <Image src="/logo.svg.jpg" alt = "logo" height={20} width={20} className="rounded-full"/>
+                                
                             <span>Tros Verse</span>
                             </Link>
+                            
                         </SidebarMenuButton>
                     </SidebarMenuItem>
                 </SidebarMenu>
             </SidebarHeader>
+                            <Separator></Separator>
+
             <SidebarContent>
                 <SidebarGroup>
-                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
                      <SidebarMenu>
                     {items.map(item=>(
                         <SidebarMenuItem key={item.title}>
                             <SidebarMenuButton asChild>
+                                
                                 <Link href={item.url}>
                                     <item.icon/>
                                     <span>{item.title}</span>
@@ -46,7 +54,22 @@ const AppSidebar = () => {
                 </SidebarGroup>
             </SidebarContent>
             <SidebarFooter>
-
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <SidebarMenuButton>
+                                        <User2/> Tro Baba <ChevronUp className="ml-auto"/>
+                                        <DropdownMenuContent align= "end" >
+                                            <DropdownMenuItem>Balance</DropdownMenuItem>
+                                            <DropdownMenuItem>Skills</DropdownMenuItem>
+                                            <DropdownMenuItem>Account</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </SidebarMenuButton>
+                                </DropdownMenuTrigger>
+                            </DropdownMenu>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
             </SidebarFooter>
         </Sidebar>
     );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,12 +15,14 @@ import Link from 'next/link';
 
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { useTheme } from "next-themes";
+import { SidebarTrigger } from "./ui/sidebar";
+
 
 const Navbar = () => {
     const {theme, setTheme}= useTheme();
     return (
         <nav className='p-4 flex items-center justify-between'>
-            collapsebutton
+            <SidebarTrigger/>
             <div className='flex items-center gap-4'>
                 <Link href="/">Dashboard</Link>
                 {/* Theme Menu */}
