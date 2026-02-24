fba91090e4aded98c2a786b4c386f9b1415e9890
Auritro <22303012@iubat.edu>
2026-02-24 15:36:39 +0600

    Charts Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import AppSidebarChart from "@/components/AppSidebarChart";
+import AreaChart from "@/components/AppAreaChart";
 
 
 export default function Home() {
   return (
     
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test 1</div>
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test 2</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppSidebarChart/></div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AreaChart/></div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test 3</div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test 4</div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test 5</div>
