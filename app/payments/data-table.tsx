f5e8a79c3657417ff6e968895e51ff711ecd9043
Auritro <22303012@iubat.edu>
2026-03-10 23:22:01 +0600

    Header redesigned

--- app/payments/data-table.tsx
+++ app/payments/data-table.tsx
@@ -34,7 +34,7 @@ export function DataTable<TData, TValue>({
   return (
     <div className="overflow-hidden rounded-md border">
       <Table>
-        <TableHeader>
+        <TableHeader className="bg-muted">
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
